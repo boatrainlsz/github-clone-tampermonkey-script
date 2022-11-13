46e81318ff6cd7498ac30036b42929be0845f5f4
boatrainlsz <yinger521999@gmail.com>
2022-11-13 14:55:30 +0800

    add comment

--- script.js
+++ script.js
@@ -9,6 +9,10 @@
 // ==/UserScript==
 (function () {
     let clipboard = document.getElementsByTagName("clipboard-copy");
+    //return is clipboard-copy is not exist
+    if (clipboard.length == 0) {
+        return
+    }
 //prepend 'git clone' to clipboard's value attribute
     clipboard[0].setAttribute("value", "git clone " + clipboard[0].getAttribute("value"));
 })();

31653546ae1b0a32885c8c94c4bafbec460a8ccb
boatrainlsz <yinger521999@gmail.com>
2022-11-13 14:53:02 +0800

    add comment

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name        github clone enhancement
+// @namespace   Violentmonkey Scripts
+// @match       https://github.com/*
+// @grant       none
+// @version     1.0
+// @author      boatrainlsz
+// @description 2020/7/1 上午10:56:13
+// ==/UserScript==
 (function () {
     let clipboard = document.getElementsByTagName("clipboard-copy");
 //prepend 'git clone' to clipboard's value attribute
